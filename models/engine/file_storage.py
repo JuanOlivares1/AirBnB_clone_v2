711be7232c3b4aa1c10549d9c0584222ca78e8bb
Juan Olivares <jdop2000@gmail.com>
2020-08-12 04:28:27 +0000

    5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            keys = FileStorage.__objects.keys()
+            objs = {}
+
+            for key in keys:
+                if cls.__name__ in key:
+                    objs[key] = FileStorage.__objects[key]
+            return objs
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +56,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it’s inside"""
+        key = type(obj).__name__ + '.' + obj.id
+
+        if key in self.all().keys():
+            del self.all()[key]
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,10 +4,18 @@ import unittest
 from models.base_model import BaseModel
 from models import storage
 import os
+import pep8
 
 
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def setUp(self):
         """ Set up test environment """
