5a7ad904e9ffc5f10bf64b58f02f7c17ad273882
Edison Girado A <1470@Holbertonschool.com>
2020-08-11 19:29:25 -0500

    pep8 testing

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-""" """
-from models.base_model import BaseModel
+"""Test cases for console class."""
 import unittest
+import pep8
 
 
-class test_console(unittest.TestCase):
-    """ """
-    pass
+class TestCodeFormat(unittest.TestCase):
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
