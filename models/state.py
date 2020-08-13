a1fecff9a63fb59b7c65d13e5c9d3aad2f2287e8
Edison Girado A <1470@Holbertonschool.com>
2020-08-12 22:27:44 -0500

    Executable files.

--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state',
+                          cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """Getter method for class attr cities."""
+        return type(self).cities
