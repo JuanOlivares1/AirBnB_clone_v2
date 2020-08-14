8576a21e2589fdb340e67f511bd59de486e17816
Juan Olivares <jdop2000@gmail.com>
2020-08-14 03:38:14 +0000

    tests for file_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-""" Module for testing file storage"""
+""" Module for testing file storage PASO"""
 import unittest
+from models.base_model import BaseModel
+from models import DBStorage
+import os
 import pep8
 
 
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
+    def test_file_storage_pep8(self):
+        """ tests pep8 compliance """
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        result = pep8style.check_files(['./models/engine/db_storage.py'])
+        self.assertEqual(result.total_errors, 0)
+
+    def test_docstring_Console(self):
+        """ tests that docstrings are present """
+        self.assertIsNotNone(DBStorage.all.__doc__)
+        self.assertIsNotNone(DBStorage.new.__doc__)
+        self.assertIsNotNone(DBStorage.save.__doc__)
+        self.assertIsNotNone(DBStorage.delete.__doc__)
+        self.assertIsNotNone(DBStorage.reload.__doc__)
