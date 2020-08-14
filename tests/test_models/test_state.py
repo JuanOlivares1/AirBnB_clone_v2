01e5c3338da5e1a20a891221e7f9b5c352b8044d
Juan Olivares <jdop2000@gmail.com>
2020-08-14 03:31:09 +0000

    tests for state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" """
+""" test """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+import pep8
 
 
 class test_state(test_basemodel):
-    """ """
+    """test_user_pep8"""
+
+    def test_user_pep8(self):
+        """ tests pep8 compliance """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['./models/state.py'])
+        self.assertEqual(result.total_errors, 0)
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
-
-    def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
