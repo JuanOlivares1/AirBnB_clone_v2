403756ccabbb87a2c12e0664a22308f2c2212ea0
Edison Girado A <1470@Holbertonschool.com>
2020-08-13 23:19:49 -0500

    Fixing tests cases.

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -2,7 +2,6 @@
 """ Module for testing file storage PASO"""
 import unittest
 from models.base_model import BaseModel
-from models import DBStorage
 import os
 import pep8
 
@@ -15,11 +14,3 @@ class test_fileStorage(unittest.TestCase):
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['./models/engine/db_storage.py'])
         self.assertEqual(result.total_errors, 0)
-
-    def test_docstring_Console(self):
-        """ tests that docstrings are present """
-        self.assertIsNotNone(DBStorage.all.__doc__)
-        self.assertIsNotNone(DBStorage.new.__doc__)
-        self.assertIsNotNone(DBStorage.save.__doc__)
-        self.assertIsNotNone(DBStorage.delete.__doc__)
-        self.assertIsNotNone(DBStorage.reload.__doc__)
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,7 +2,6 @@
 """ Module for testing file storage PASO"""
 import unittest
 from models.base_model import BaseModel
-from models import DBStorage
 import os
 import pep8
 
@@ -10,20 +9,8 @@ import pep8
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
-    # def setUp(self):
-    #     """ Set up test environment """
-    #     self.db = DBStorage()
-
     def test_file_storage_pep8(self):
         """ tests pep8 compliance """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['./models/engine/db_storage.py'])
         self.assertEqual(result.total_errors, 0)
-
-    def test_docstring_Console(self):
-        """ tests that docstrings are present """
-        self.assertIsNotNone(DBStorage.all.__doc__)
-        self.assertIsNotNone(DBStorage.new.__doc__)
-        self.assertIsNotNone(DBStorage.save.__doc__)
-        self.assertIsNotNone(DBStorage.delete.__doc__)
-        self.assertIsNotNone(DBStorage.reload.__doc__)
