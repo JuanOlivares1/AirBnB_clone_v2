23a8ececa1d99e98cf3d11e88e5e9d25607df83b
Juan Olivares <jdop2000@gmail.com>
2020-08-14 03:28:48 +0000

    tests for amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" """
+"""tesst models/amenity.py"""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import pep8
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """test_Amenity"""
+
+    def test_amenity_pep8(self):
+        """ tests pep8 compliance """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['./models/amenity.py'])
+        self.assertEqual(result.total_errors, 0)
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
-
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
