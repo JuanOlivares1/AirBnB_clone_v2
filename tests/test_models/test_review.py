b86987325aac69fbcc33603792a7a2c23b7350ca
Juan Olivares <jdop2000@gmail.com>
2020-08-14 03:30:50 +0000

    tests for review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-""" """
+"""test review"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+import pep8
 
 
 class test_review(test_basemodel):
-    """ """
+    """test_review"""
+
+    def test_Review_pep8(self):
+        """ tests pep8 compliance """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['./models/review.py'])
+        self.assertEqual(result.total_errors, 0)
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
-
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
-
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
-
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
