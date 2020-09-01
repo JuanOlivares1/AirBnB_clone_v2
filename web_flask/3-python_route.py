29525160124d706268057f45c01baa72c3d14da1
Juan Olivares <jdop2000@gmail.com>
2020-09-01 20:12:49 +0000

    using route arguments and defining more routes

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Getting started on flask"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Defines response for route '/'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Defines response for route '/hbnb'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Defines response for route '/c/<text>'"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Defines response for route '/python' and '/python/<text>'"""
+    return "Python {}".format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run()
