cd2af9542e3b24b5c058d668611a6909dec89984
Juan Olivares <jdop2000@gmail.com>
2020-09-01 20:12:12 +0000

    fixing comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Defines response for route '\'"""
+    """Defines response for route '/'"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Defines response for route '\'"""
+    """Defines response for route '/'"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Defines response for route '\hbnb'"""
+    """Defines response for route '/hbnb'"""
     return 'HBNB'
 
 if __name__ == '__main__':
