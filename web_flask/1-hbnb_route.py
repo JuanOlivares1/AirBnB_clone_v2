88040c064081f84d6051343bbda15330556fdced
Juan Olivares <jdop2000@gmail.com>
2020-09-01 19:53:50 +0000

    Defining different rutes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,6 +10,7 @@ def hello_hbnb():
     """Defines response for route '\'"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Defines response for route '\hbnb'"""
