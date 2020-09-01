79888e37d623bfbce4560196e983b6175d2f0dfd
Juan Olivares <jdop2000@gmail.com>
2020-09-01 19:40:48 +0000

    getting started with flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Defines response for route '\'"""
     return 'Hello HBNB!'
-
-if __name__ == '__main__':
-    app.run()
