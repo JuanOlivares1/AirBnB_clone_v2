6c68eedea31c027a7a692f3a91cecef231808e33
Juan Olivares <jdop2000@gmail.com>
2020-09-01 19:52:54 +0000

    Defining different rutes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Getting started on flask"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Defines response for route '\'"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Defines response for route '\hbnb'"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run()
